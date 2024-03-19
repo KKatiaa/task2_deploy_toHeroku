0e9212ffb4b50c0529ce9908ba7c529a5a6ffa00
KKatiaa <katyakravchuk123@gmail.com>
2024-03-19 17:40:32 +0200

    edited hello world

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var http = require('http');
 const PORT = process.env.PORT || 3000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World! Hello World! Hello World! Hello World!');
+  res.end('Hello World! Hello World! Hello World!');
 }).listen(PORT);
