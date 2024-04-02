9a8f9b94c72624554a026fdf1c50c4ca2cbe8eb5
KKatiaa <katyakravchuk123@gmail.com>
2024-04-02 17:10:34 +0300

    added changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
-const PORT = process.env.PORT || 81;
+const PORT = process.env.PORT || 80;
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    res.end('Hello World! Added changes');
 }).listen(PORT);
